78f457f03ce9bb10b95efa2ef4cab1052e1954a1
foryoonas <seol_ya0416@naver.com>
2024-12-05 15:58:47 +0900

    6주차~13주차 api 실습

--- 6week/fapi1.py
+++ 6week/fapi1.py
@@ -71,6 +71,34 @@ async def generate_qr(data: str = Form(...)):
     
     return StreamingResponse(BytesIO(img_byte_arr), media_type="image/png")
 
+def loginDB(userId,userPassword):
+    import sqlite3
+    
+    conn = sqlite3.connect('education.db')
+    cursor = conn.cursor()
+    
+    query = 'SELECT * FROM user WHERE userId = ? AND userPassword = ?'
+    cursor.execute(query,(userId,userPassword))
+    result = cursor.fetchone()
+    conn.close()
+    
+    if result:
+        print("Login successful!")
+        print("User Info:",result) #쿼리 결과 출력
+        return True
+    else:
+        print("Login failed.Invalid username or password.")
+        return False
+    
+@app.post("/login")
+def login_form(userid: str = Form(...), userpassword: str = Form(...)):
+    result = loginDB (userid,userpassword)
+    
+    if result == True :
+        return {"msg": f"{userid}님 반갑습니다."}    
+    else :
+        return {"msg": f"로그인에 실패했습니다."}
+
 from fastapi.staticfiles import StaticFiles
 app.mount("/", StaticFiles(directory="static", html=True),name="static")
 
